8b8177fa13c75f95ae38a172293a04a73e79f889
Anas Mamlouk <anas.mamlouk@yahoo.fr>
2024-04-01 13:47:09 +0200

    init ts

---
+++ myfiles.ts
@@ -0,0 +1,12 @@
+type ShopOfNumbers = { 
+    name : string
+    items : Array<number>
+}
+type ShopOfStrings = { 
+    name : string
+    items : Array<string>
+}
+type ShopOfBooleans = { 
+    name : string
+    items : Array<boolean>
+}
