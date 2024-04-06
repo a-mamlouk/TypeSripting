bf74f652c886ab86147a759767dac3bf296ad469
Anas Mamlouk <anas.mamlouk@yahoo.fr>
2024-04-07 01:35:40 +0200

    0135070424

--- Udemy scripts/day1.ts
+++ Udemy scripts/day1.ts
@@ -2,3 +2,9 @@
 const Value: number = 5;
 const Name: string = "Anas";
 let aStringOrANumber: string | number = "";
+let gift: undefined | string;
+console.log(gift);
+let present = "";
+console.log(present);
+let friends: string | null = "car";
+console.log(friends);
