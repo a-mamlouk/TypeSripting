6292e203ef201f101df47c947bc31015f560d624
Anas Mamlouk <anas.mamlouk@yahoo.fr>
2024-04-07 01:50:32 +0200

    0150070424

---
+++ Udemy scripts/day2.ts
@@ -0,0 +1,12 @@
+const score1 = 50;
+const score2 = 100;
+const FinalScore = score1 + score2;
+
+let Age = 50;
+const YearsPassed = 5;
+Age -= YearsPassed;
+console.log(Age);
+const NewAge = Age - YearsPassed;
+console.log(NewAge);
+console.log(NewAge, FinalScore);
+console.log(4 + 4 - 2 ** 3);
